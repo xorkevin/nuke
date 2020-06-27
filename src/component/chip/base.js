d3fabb95e14248c8f662ee85082f3e64538bbc3f
Kevin Wang <kevin@xorkevin.com>
2020-06-27 00:09:33 -0700

    Impl form multiselect

--- src/component/chip/base.js
+++ src/component/chip/base.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Chip = ({className, children}) => {
+const Chip = ({className, onClick, children}) => {
   const k = ['chip'];
   if (className) {
     k.push(className);
   }
 
-  return <span className={k.join(' ')}>{children}</span>;
+  return (
+    <span className={k.join(' ')} onClick={onClick}>
+      {children}
+    </span>
+  );
 };
 
 export default Chip;
