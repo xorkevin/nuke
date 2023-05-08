0eb24c9ce3b190223e02b01bd5e8d34f29dc7167
Kevin Wang <kevin@xorkevin.com>
2023-05-07 21:19:46 -0700

    Add css modules types

---
+++ packages/nuke/lib/component/button/base.tsx
@@ -0,0 +1,51 @@
+import type {MouseEventHandler, Ref, ReactNode} from 'react';
+import styles from './styles.module.css';
+
+type Props = {
+  id?: string;
+  className?: string;
+  fullWidth?: boolean;
+  fixedWidth?: boolean;
+  disabled?: boolean;
+  label?: string;
+  onClick?: MouseEventHandler;
+  forwardedRef?: Ref<HTMLButtonElement>;
+  children?: ReactNode;
+};
+
+const Button: React.FC<Props> = ({
+  id,
+  className,
+  fullWidth,
+  fixedWidth,
+  disabled,
+  label,
+  onClick,
+  forwardedRef,
+  children,
+}) => {
+  const j = [styles['button']];
+  if (fullWidth) {
+    j.push('full-width');
+  }
+  if (fixedWidth) {
+    j.push('fixed-width');
+  }
+  if (className) {
+    j.push(className);
+  }
+  return (
+    <button
+      ref={forwardedRef}
+      id={id}
+      className={j.join(' ')}
+      disabled={disabled}
+      onClick={onClick}
+      aria-label={label}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
