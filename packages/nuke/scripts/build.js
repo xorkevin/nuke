618d460167b81526533b03cdb6fd2ba65bd8615b
Kevin Wang <kevin@xorkevin.com>
2023-05-16 01:32:36 -0700

    Improve css copy build script

--- packages/nuke/scripts/build.js
+++ packages/nuke/scripts/build.js
@@ -2,12 +2,71 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 async function* walk(dir) {
-  for await (const d of await fs.promises.opendir(dir)) {
-    const entry = path.join(dir, d.name);
-    if (d.isDirectory()) {
+  for await (const f of await fs.promises.opendir(dir)) {
+    const entry = path.join(dir, f.name);
+    if (f.isDirectory()) {
       yield* walk(entry);
-    } else if (d.isFile()) {
+    } else if (f.isFile()) {
       yield entry;
+    }
+  }
+}
+
+async function statFile(name) {
+  try {
+    const s = await fs.promises.stat(name);
+    return [s, null];
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return [null, new Error(`File ${name} does not exist`)];
+    }
+    throw err;
+  }
+}
+
+async function filesEqual(fname1, fname2) {
+  {
+    const [s1, err1] = await statFile(fname1);
+    if (err1 !== null) {
+      // file 1 is assumed to exist
+      throw err1;
+    }
+    const [s2, err2] = await statFile(fname2);
+    if (err2 !== null) {
+      // file 2 may not exist
+      return false;
+    }
+    if (s1.size !== s2.size) {
+      return false;
+    }
+  }
+
+  const bufSize = 1024 * 16;
+  let f1, f2;
+  try {
+    f1 = await fs.promises.open(fname1);
+    f2 = await fs.promises.open(fname2);
+    const buf1 = Buffer.alloc(bufSize);
+    const buf2 = Buffer.alloc(bufSize);
+    while (true) {
+      const [r1, r2] = await Promise.all([f1.read(buf1), f2.read(buf2)]);
+      if (r1.bytesRead !== r2.bytesRead) {
+        throw new Error('Mismatched file reads');
+      }
+      if (buf1.compare(buf2, 0, r2.bytesRead, 0, r1.bytesRead) !== 0) {
+        return false;
+      }
+      if (r1.bytesRead < bufSize) {
+        break;
+      }
+    }
+    return true;
+  } finally {
+    if (f1) {
+      await f1.close();
+    }
+    if (f2) {
+      await f2.close();
     }
   }
 }
@@ -15,7 +74,13 @@ async function* walk(dir) {
 const cssRegex = /\.module\.css$/;
 for await (const p of walk('lib')) {
   if (cssRegex.test(p)) {
-    console.log(`copying ${p}`);
-    await fs.promises.copyFile(p, path.join('dist', p));
+    const dest = path.join('dist', p);
+    const destIsEqual = await filesEqual(p, dest);
+    if (destIsEqual) {
+      console.log(`${p} matches ${dest}; skipping`);
+      continue;
+    }
+    console.log(`copying ${p} to ${dest}`);
+    await fs.promises.copyFile(p, dest);
   }
 }
